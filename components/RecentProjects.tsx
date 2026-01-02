cc3689a11d30a8a06c3511aab1e9dcb31b0f989b
Karan Nayal <somnathtigers2006@gmail.com>
2026-01-02 14:04:54 +0530

    Code UPDATED

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -22,10 +22,7 @@ const RecentProjects = () => {
               href={item.link}
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
-                >
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162D]">
                   <img src="/bg.png" alt="bgimg" />
                 </div>
                 <img
@@ -39,13 +36,7 @@ const RecentProjects = () => {
                 {item.title}
               </h1>
 
-              <p
-                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                style={{
-                  color: "#BEC1DD",
-                  margin: "1vh 0",
-                }}
-              >
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2 text-[#BEC1DD] my-2">
                 {item.des}
               </p>
 
@@ -56,7 +47,8 @@ const RecentProjects = () => {
                       key={index}
                       className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                       style={{
-                        transform: `translateX(-${5 * index + 2}px)`,
+                        transform: `translateX(calc(-5 * ${index}px - 2px))`,
+                        marginLeft: index > 0 ? `-${5 * index + 2}px` : '0',
                       }}
                     >
                       <img src={icon} alt="icon5" className="p-2" />
